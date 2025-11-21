efa370ee1a1e58302825d92eb28af614ebd10d27
Natalia Lewis <a02296603@usu.edu>
2025-11-21 16:02:25 -0700

    Added a fifth layer to the CNN because my data was too complex

--- model.py
+++ model.py
@@ -35,16 +35,21 @@ class DogBreedCNN(nn.Module):
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(256)
         self.pool4 = nn.MaxPool2d(2, 2)
+
+        # Fifth convolutional block that I added later
+        self.conv5 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.pool5 = nn.MaxPool2d(2, 2)
         
         # Calculates the size of the flattened features to feed into the first fully connected layer
-        self.fc1_input_size = 256 * 14 * 14
+        self.fc1_input_size = 512 * 7 * 7
         
         # Fully connected layers
-        self.fc1 = nn.Linear(self.fc1_input_size, 512)
+        self.fc1 = nn.Linear(self.fc1_input_size, 1024)
         self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(1024, 512)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(256, num_classes)
+        self.fc3 = nn.Linear(512, num_classes)
         
     def forward(self, x):
         """
@@ -56,6 +61,7 @@ class DogBreedCNN(nn.Module):
         x = self.pool2(F.relu(self.bn2(self.conv2(x))))
         x = self.pool3(F.relu(self.bn3(self.conv3(x))))
         x = self.pool4(F.relu(self.bn4(self.conv4(x))))
+        x = self.pool5(F.relu(self.bn5(self.conv5(x))))
         
         # Flatten for fully connected layers
         x = x.view(-1, self.fc1_input_size)
